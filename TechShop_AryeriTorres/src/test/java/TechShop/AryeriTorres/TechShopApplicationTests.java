c2539bcb385d9c653c61d7d47ab1490e08f7d75b
Aryeri Torres <yeye120625@gmail.com>
2026-05-20 20:10:28 -0600

    Commit 1
    Creación del proyecto. Semana 1 y 2

---
+++ TechShop_AryeriTorres/src/test/java/TechShop/AryeriTorres/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.AryeriTorres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
