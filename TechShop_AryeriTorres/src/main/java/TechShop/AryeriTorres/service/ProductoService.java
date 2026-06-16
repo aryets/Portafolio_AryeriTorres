9da7263af79b2d34e01b0c1ba9d80ec5983fd087
Aryeri Torres <yeye120625@gmail.com>
2026-06-15 20:50:30 -0600

    Semana #5.2
    Presentacion semana 5

---
+++ TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/service/ProductoService.java
@@ -0,0 +1,61 @@
+package TechShop.AryeriTorres.service;
+
+import TechShop.AryeriTorres.domain.Producto;
+import TechShop.AryeriTorres.repository.ProductoRepository;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+
+    private final ProductoRepository productoRepository;
+    private final FirebaseStorageService firebaseStorageService;
+
+    public ProductoService(ProductoRepository productoRepository, FirebaseStorageService firebaseStorageService) {
+        this.productoRepository = productoRepository;
+        this.firebaseStorageService = firebaseStorageService;
+    }
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        }
+        return productoRepository.findAll();
+    }
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+    @Transactional
+    public void save(Producto producto, MultipartFile imagenFile) {
+        producto = productoRepository.save(producto);
+        if (!imagenFile.isEmpty()) { // Si no está vacío... pasaron una imagen...
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile, "producto",
+                        producto.getIdProducto());
+                producto.setRutaImagen(rutaImagen);
+                productoRepository.save(producto);
+            } catch (IOException e) {
+                // Manejo de excepción si falla la subida de imagen
+            }
+        }
+    }
+    @Transactional
+    public void delete(Integer idProducto) {
+        // Verifica si el producto existe antes de intentar eliminarlo
+        if (!productoRepository.existsById(idProducto)) {
+            throw new IllegalArgumentException("El producto con ID " + idProducto + " no existe.");
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar el producto. Tiene datos asociados.", e);
+        }
+    }
+}
