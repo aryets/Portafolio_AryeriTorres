c3f295ef12c67b9d0ec0f8e822da6cb943670fc5
Aryeri Torres <yeye120625@gmail.com>
2026-06-15 19:27:10 -0600

    Semana #5
    Presentación Lección 4

--- TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/domain/Categoria.java
+++ TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/domain/Categoria.java
@@ -34,4 +34,5 @@ public class Categoria implements Serializable {
 
     @Column(name = "activo")
     private Boolean activo;
+
 }
--- TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/service/CategoriaService.java
+++ TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/service/CategoriaService.java
@@ -2,28 +2,66 @@ package TechShop.AryeriTorres.service;
 
 import TechShop.AryeriTorres.domain.Categoria;
 import TechShop.AryeriTorres.repository.CategoriaRepository;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CategoriaService {
 
+    // El repositorio es final para asegurar la inmutabilidad
     private final CategoriaRepository categoriaRepository;
 
-    // Inyección de dependencias vía constructor
+    // Inyección por constructor (No requiere @Autowired en Spring moderno)
     public CategoriaService(CategoriaRepository categoriaRepository) {
         this.categoriaRepository = categoriaRepository;
     }
-    
+
     @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activo) {
-        if (activo) {
-            // Devuelve solo las categorías activas
+        if (activo) { // Sólo activos...
             return categoriaRepository.findByActivoTrue();
-        } else {
-            // Devuelve todas las categorías
-            return categoriaRepository.findAll();
+        }
+        return categoriaRepository.findAll();
+    }
+    
+    @Transactional(readOnly = true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
+    }
+ /*
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (!imagenFile.isEmpty()) { //Si no está vacío... pasaron una imagen...            
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile, "categoria",
+                        categoria.getIdCategoria());
+                categoria.setRutaImagen(rutaImagen);
+                categoriaRepository.save(categoria);
+            } catch (IOException e) {
+
+            }
         }
     }
+    
+    @Transactional
+    public void delete(Integer idCategoria) {
+        // Verifica si la categoría existe antes de intentar eliminarlo
+        if (!categoriaRepository.existsById(idCategoria)) {
+            // Lanza una excepción para indicar que el usuario no fue encontrado
+            throw new IllegalArgumentException("La categoría con ID " + idCategoria + " no existe.");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            // Lanza una nueva excepción para encapsular el problema de integridad de datos
+            throw new IllegalStateException("No se puede eliminar la categoria. Tiene datos asociados.", e);
+        }
+    }*/
 }
