e465a34e3081f278916baa44747f2706d874225d
Aryeri Torres <yeye120625@gmail.com>
2026-07-09 16:32:55 -0600

    Semana #9

---
+++ TechShop_AryeriTorres/src/main/java/TechShop/AryeriTorres/SecurityConfig.java
@@ -0,0 +1,60 @@
+
+package TechShop.AryeriTorres;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public static final String[] PUBLIC_URLS = {
+        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
+        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
+    };
+
+    public static final String[] USUARIO_URLS = {
+        "/facturar/carrito"
+    };
+
+    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+        "/producto/listado", "/categoria/listado", "/usuario/listado"
+    };
+
+    public static final String[] ADMIN_URLS = {
+        "/producto/**", "/categoria/**", "/usuario/**"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    // Este método será reemplazado la siguiente semana
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        UserDetails juan = User.builder()
+                .username("juan")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+        UserDetails rebeca = User.builder()
+                .username("rebeca")
+                .password(passwordEncoder.encode("456"))
+                .roles("VENDEDOR")
+                .build();
+        UserDetails pedro = User.builder()
+                .username("pedro")
+                .password(passwordEncoder.encode("789"))
+                .roles("USUARIO")
+                .build();
+        return new InMemoryUserDetailsManager(juan, rebeca, pedro);
+    }
+}
